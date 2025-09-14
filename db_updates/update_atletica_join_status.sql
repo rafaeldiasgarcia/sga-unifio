209697c3c0749c132002435248c1d6911abaff41
rafaeldiasgarcia <rafdgar@gmail.com>
2025-09-13 22:34:19 -0300

    configuração de databases

--- db_updates/update_atletica_join_status.sql
+++
@@ -1,2 +0,0 @@
--- Atualiza o enum atletica_join_status para incluir o status 'aprovado'
-ALTER TABLE usuarios MODIFY COLUMN atletica_join_status ENUM('none', 'pendente', 'aprovado') DEFAULT 'none';
